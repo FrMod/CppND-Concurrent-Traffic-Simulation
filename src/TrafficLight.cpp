2fd64cc543e87c2e75868458d4ea4153bcdb0cae
FrMod <francesco.modulo@gmail.com>
2023-01-29 15:08:16 +0100

    Implemented cycleThroughPhases and simulate

--- src/TrafficLight.cpp
+++ src/TrafficLight.cpp
@@ -40,10 +40,11 @@ TrafficLightPhase TrafficLight::getCurrentPhase()
 {
     return _currentPhase;
 }
-
+*/
 void TrafficLight::simulate()
 {
     // FP.2b : Finally, the private method „cycleThroughPhases“ should be started in a thread when the public method „simulate“ is called. To do this, use the thread queue in the base class. 
+    threads.emplace_back(std::thread(&TrafficLight::cycleThroughPhases, this));
 }
 
 // virtual function which is executed in a thread
@@ -52,7 +53,29 @@ void TrafficLight::cycleThroughPhases()
     // FP.2a : Implement the function with an infinite loop that measures the time between two loop cycles 
     // and toggles the current phase of the traffic light between red and green and sends an update method 
     // to the message queue using move semantics. The cycle duration should be a random value between 4 and 6 seconds. 
-    // Also, the while-loop should use std::this_thread::sleep_for to wait 1ms between two cycles. 
+    // Also, the while-loop should use std::this_thread::sleep_for to wait 1ms between two cycles.
+
+    // https://stackoverflow.com/questions/13445688/how-to-generate-a-random-number-in-c 
+    std::random_device dev;
+    std::mt19937 rng(dev());
+    std::uniform_int_distribution<std::mt19937::result_type> dist_time_sec(4,6); // distribution in range [4, 6]
+    auto t_start = std::chrono::high_resolution_clock::now();
+    int target_time_interval = dist_time_sec(rng);
+    while (true){
+        // https://stackoverflow.com/questions/728068/how-to-calculate-a-time-difference-in-c        
+        double elapsed_time_ms = std::chrono::duration<double, std::milli>(std::chrono::high_resolution_clock::now()-t_start).count();
+        if(elapsed_time_ms >= target_time_interval) {
+            if(this->getCurrentPhase() == TrafficLightPhase::red){
+                this->_currentPhase = TrafficLightPhase::green;
+            }
+            else{
+                this->_currentPhase = TrafficLightPhase::green;
+            }
+            target_time_interval = dist_time_sec(rng);
+        }
+        t_start = std::chrono::high_resolution_clock::now();
+        std::this_thread::sleep_for(std::chrono::milliseconds(1));
+    }
+   
 }
 
-*/
