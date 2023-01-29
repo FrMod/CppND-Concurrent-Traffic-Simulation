73c210a2275ad0fdaa6e959e8ea9316524d9236b
FrMod <francesco.modulo@gmail.com>
2023-01-29 15:31:45 +0100

    Implemented receive

--- src/TrafficLight.cpp
+++ src/TrafficLight.cpp
@@ -4,15 +4,20 @@
 
 /* Implementation of class "MessageQueue" */
 
-/* 
+ 
 template <typename T>
 T MessageQueue<T>::receive()
 {
     // FP.5a : The method receive should use std::unique_lock<std::mutex> and _condition.wait() 
     // to wait for and receive new messages and pull them from the queue using move semantics. 
     // The received object should then be returned by the receive function. 
+    std::unique_lock<std::mutex> uLock(_mutex);
+    _cond.wait(uLock, [this] {return ! _queue.empty();});
+    T msg = std::move(_queue.back());
+    _queue.pop_back();
+    return msg;
 }
-*/
+
 template <typename T>
 void MessageQueue<T>::send(T &&msg)
 {
@@ -22,7 +27,6 @@ void MessageQueue<T>::send(T &&msg)
     _queue.push_back(std::move(msg));
     _cond.notify_one();
 }
-
 
 /* Implementation of class "TrafficLight" */
 
